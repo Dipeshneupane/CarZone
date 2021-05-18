a5b4b90d2f2fec41f35cfb013d571e7e82e9c2b3
Dipesh Neupane <dipeshneupane2055@gmail.com>
2021-05-18 10:02:01 +0545

    making dynamic templates

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name = 'home'),
+    path('about', views.about, name = 'about'),
+    path('services', views.services, name = 'services'),
+    path('contact', views.contact, name = 'contact'),
 ]
